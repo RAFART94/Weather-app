2b7996c4f4aaac5cbbc6b40c5e62ca0c59d7641b
EstebanQraneos <143415049+EstebanQraneos@users.noreply.github.com>
2024-04-02 12:43:54 +0100

    Assert en el test CityInfo

--- src/components/CityInfo/CityInfo.test.jsx
+++ src/components/CityInfo/CityInfo.test.jsx
@@ -10,11 +10,21 @@ test('cityInfo render', async () => {
     const { findAllByRole } = render(<CityInfo city='Las Palmas de Gran Canaria' country='España' />)
 
     // Act
+    const city = 'Las Palmas de Gran Canaria'
+    const country = 'España'
     // findAllByRole nos va a buscar (en este caso) todos los componentes que sean
     // 'heading' => H1, H2, H3.. etc
     // El resultado es un array de componentes (cityAndCountryComponents)
     const cityAndCountryComponents = await findAllByRole('heading')
-    
+
     // Assert
+    // Cuándo el test va a ser correcto?
+    // Definición: Cuando en el primer elemento (heading) se encuentre la ciudad 'Las Palmas de GC'
+    // y cuando en el segundo elemento se encuentre el país 'España'
+
+    // el motor de 'Jest' provee esta palabra para el 'Assert' de los test
+    expect(cityAndCountryComponents[0]).toHaveTextContent(city)
+    expect(cityAndCountryComponents[1]).toHaveTextContent(country)
+    // Si estas condiciones se cumplen (expect), el test esta 'ok'
 
 })
